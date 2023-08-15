a8fda4d80d02535beaeb221bb71b58d1f063e3ee
devkjoon <devkjoon@gmail.com>
2023-08-15 15:50:43 -0400

    assigned ids to html tags, adding functionality in js for data retrieval

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,22 @@ function load() {
     navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude;
         let long = position.coords.longitude;
-        console.log(lat, long);
+        let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&${long}&appid=76d52450ab94e81993d2613d091c301e&units=imperial`
+        
+        fetch(fiveDayURL)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            fiveDayWeather(data);
+            localStorage.setItem("response", JSON.stringify(data.city.name));
+            loadURL();
+        })
     })
+}
+
+function loadURL() {
+    let cityName = JSON.parse(localStorage.getItem("response"));
+
+    let 
 }
